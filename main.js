0e392555c62a1957522c7f97d9ecdaa7faec5d45
Ossama Qureshi <ossamasq1@outlook.com>
2022-09-22 15:32:15 -0400

    Form Revisions

--- main.js
+++ main.js
@@ -53,30 +53,6 @@ function toggleContrast() {
   }
 }
 
-function contact(event) {
-  event.preventDefault();
-  const loading = document.querySelector(".modal__overlay--loading");
-  const success = document.querySelector(".modal__overlay--success");
-  loading.classList += " modal__overlay--visible";
-  emailjs
-    .sendForm(
-      "service_80ih0if",
-      "template_d9refyl",
-      event.target,
-      "user_K1PoFs8pB2YVWStDxrUls"
-    )
-    .then(() => {
-      loading.classList.remove("modal__overlay--visible");
-      success.classList += " modal__overlay--visible";
-    })
-    .catch(() => {
-      loading.classList.remove("modal__overlay--visible");
-      alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
-      );
-    });
-}
-
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false;
