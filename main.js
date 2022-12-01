98f0705904529ddbaf09a7b0ef47a312dea00173
Ossama Qureshi <ossamasq1@outlook.com>
2022-12-01 16:00:04 -0500

    Projects Updated

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-const hamburger = document.querySelector('.hamburger');
-const hamburger_icon = hamburger.querySelector('span');
-const mobile_menu = document.querySelector('.mobile-menu');
+const hamburger = document.querySelector(".hamburger");
+const hamburger_icon = hamburger.querySelector("span");
+const mobile_menu = document.querySelector(".mobile-menu");
 
-hamburger.addEventListener('click', () => {
-	hamburger_icon.innerText = hamburger_icon.innerText === 'menu' 
-		? 'close'
-		: 'menu';
-	
-	mobile_menu.classList.toggle('is-open');
-})
+hamburger.addEventListener("click", () => {
+  hamburger_icon.innerText =
+    hamburger_icon.innerText === "menu" ? "close" : "menu";
+
+  mobile_menu.classList.toggle("is-open");
+});
 
 // Carousel Slider
-var swiper = new Swiper('.blog-slider', {
-	spaceBetween: 30,
-	effect: 'fade',
-	loop: true,
-	mousewheel: {
-	  invert: false,
-	},
-	// autoHeight: true,
-	pagination: {
-	  el: '.blog-slider__pagination',
-	  clickable: true,
-	}
-  });
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+});
 
 // Projects Section and Contact Me Modal
 let isModalOpen = false;
@@ -39,17 +38,18 @@ function moveBackground(event) {
     const isOdd = i % 2 !== 0;
     const boolInt = isOdd ? -1 : 1;
     // Added rotate after tutorial
-    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${
+      y * boolInt
+    }px) rotate(${x * boolInt * 10}deg)`;
   }
 }
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
   if (contrastToggle) {
-    document.body.classList += " dark-theme"
-  }
-  else {
-    document.body.classList.remove("dark-theme")
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
   }
 }
 
@@ -62,13 +62,12 @@ function toggleModal() {
   document.body.classList += " modal--open";
 }
 
-
 // Scroll Animation
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e){
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
     document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior : "smooth"
+      behavior: "smooth",
     });
   });
 });
